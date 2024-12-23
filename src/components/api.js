cb2520ac3173c0b28e3062af3c7f0b4491dcdaae
MariaYak <mariayaklushina@gmail.com>
2024-12-23 19:47:53 +0500

    webpack-token-fix-commit

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,8 @@
 const config = {
     baseUrl: 'https://nomoreparties.co/v1/frontend-st-cohort-201',
+    groupdID: 'apf-cohort-202',
     headers: {
-      authorization: 'b9150b16-1a08-44d7-a100-9603c9e6e81c',
+      authorization: '74d7ea3f-ad4d-462b-a0bf-9af4dcdd2905',
       'Content-Type': 'application/json'
     }
   }
